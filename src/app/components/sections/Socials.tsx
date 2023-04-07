c6f91562db3e848e6d81daa2de06177710a4eec0
Gabriel Taveira <gabrielstaveira@gmail.com>
2023-04-07 19:33:18 -0300

    fix: darkmode for socials

--- src/app/components/sections/Socials.tsx
+++ src/app/components/sections/Socials.tsx
@@ -7,7 +7,7 @@ export const Socials = () => (
         key={social.name}
         href={social.url}
         target="_blank"
-        className="flex gap-2 items-center text-zinc-500 hover:text-zinc-700 dark:hover:text-zinc-300 p-5 bg-zinc-100 w-full justify-center max-w-md"
+        className="flex gap-2 items-center text-zinc-500 hover:text-zinc-700 dark:hover:text-zinc-300 p-5 bg-zinc-100 dark:bg-zinc-900 dark:text-zinc-200 w-full justify-center max-w-md"
       >
         <span>{social.name}</span>
       </a>
