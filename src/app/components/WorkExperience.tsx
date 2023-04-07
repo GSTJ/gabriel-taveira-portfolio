315e00b9d8d89987ed3cbbb6af3adf1061e532a9
Gabriel Taveira <gabrielstaveira@gmail.com>
2023-04-07 12:11:24 -0300

    feat: render work experiences

---
+++ src/app/components/WorkExperience.tsx
@@ -0,0 +1,47 @@
+import { WorkExperienceData } from "../consts/workExperience";
+
+const WorkExperience: React.FC<WorkExperienceData> = ({
+  startDate,
+  endDate,
+  companyName,
+  subCompanyName,
+  jobTitle,
+  jobDescription,
+  technologies,
+  jobType,
+}) => {
+  return (
+    <div className="flex">
+      <p className="font-medium flex-shrink-0 text-right mr-4">
+        {startDate}
+        <br />/ {endDate ? endDate : "Now"}
+      </p>
+      <div className="whitespace-nowrap">
+        <div className="flex justify-between mb-5 flex-col gap-4 md:flex-row md:gap-0">
+          <div>
+            <div className="flex">
+              <p className="font-semibold mr-1">{companyName}</p>
+              <p className="text-gray-400">
+                {subCompanyName ? `/ ${subCompanyName}` : ""}
+              </p>
+            </div>
+            <p>{jobTitle}</p>
+          </div>
+          <div className="flex items-end gap-2 flex-wrap">
+            {jobType && (
+              <div className="bg-zinc-900 h-9 pl-4 pr-4 flex w-fit items-center text-gray-500 text-sm">
+                {jobType}
+              </div>
+            )}
+            <div className="bg-zinc-900 h-9 pl-4 pr-4 flex items-center text-gray-300 whitespace-nowrap">
+              {technologies?.join(" / ")}
+            </div>
+          </div>
+        </div>
+        <p className="text-gray-400 whitespace-pre-line">{jobDescription}</p>
+      </div>
+    </div>
+  );
+};
+
+export default WorkExperience;
---
+++ src/app/consts/workExperience.ts
@@ -0,0 +1,72 @@
+export interface WorkExperienceData {
+  startDate: string;
+  endDate?: string;
+  companyName: string;
+  subCompanyName?: string;
+  jobTitle: string;
+  jobDescription: string;
+  technologies?: string[];
+  jobType?: "Contractor";
+}
+
+export default [
+  {
+    startDate: "2022",
+    companyName: "Meta",
+    subCompanyName: "Kustomer (New York)",
+    jobTitle: "Senior Software Engineer",
+    jobType: "Contractor",
+    technologies: ["React", "Node.JS"],
+
+    jobDescription: `At Meta, I work within Kustomer, a platform that allows businesses to manage all their communication channels in one place. Here, I develop applications and provide feedback on UX to improve the platform.
+    
+    Currently, I am working on a design system that will be open-sourced, enabling external developers to develop standardized applications that align with our brand.
+    
+    This makes our application review process easier, provides a better, faster and more consistent development experience, and offers a more consistent application experience for end-users.`,
+  },
+  {
+    startDate: "2022",
+    companyName: "X-Team",
+    subCompanyName: "Groundswell (California)",
+    jobTitle: "Senior Software Engineer",
+    technologies: ["React", "React Native", "Node.JS"],
+    jobDescription: `At X-Team, I work for a fintech company in California called Groundswell, developing a donation platform for businesses and employees. My focus is on the main application, which is built using React Native.
+    
+    I aim to help people and promote a culture of robust testing to ensure that anyone can safely refactor our code.
+    
+    As a financial platform, we have implemented various security standards, such as SOC2. I am currently working on a complete platform rewrite, which involves switching from GO to Node.JS stack.`,
+  },
+  {
+    startDate: "2020",
+    endDate: "2022",
+    companyName: "Zé Delivery",
+    subCompanyName: "Anheuser-Busch InBev",
+    jobTitle: "Frontend Engineer III",
+    technologies: ["React", "React Native", "Node.JS"],
+    jobDescription: `At X-Team, I work for a fintech company in California called Groundswell, developing a donation platform for businesses and employees. My focus is on the main application, which is built using React Native.
+      
+      I aim to help people and promote a culture of robust testing to ensure that anyone can safely refactor our code.
+      
+      As a financial platform, we have implemented various security standards, such as SOC2. I am currently working on a complete platform rewrite, which involves switching from GO to Node.JS stack.`,
+  },
+  {
+    startDate: "2019",
+    endDate: "2020",
+    companyName: "Alfred Delivery",
+    jobTitle: "Software Engineer",
+    technologies: ["React Native", "Node.JS"],
+    jobDescription: ` Alfred Delivery is a delivery app focused on more miniature cities and remote areas in Brazil. It currently has franchises in over 90 cities.
+
+I was responsible for maintaining and adding minor features to our production app in Ionic 3. After a few months, I got to rewrite the app in React Native, entirely revamping the interface while drastically improving performance.`,
+  },
+  {
+    startDate: "2015",
+    endDate: "2019",
+    companyName: "Micro Import Group",
+    jobTitle: "Full Stack Developer",
+    technologies: ["React", "Node.JS", "PHP"],
+    jobDescription: `Micro Import is a Premium Apple Service Provider in Brazil, inside the state of São Paulo - The best and first one in the region.
+
+    While there, I got to go from an authorized technician to a developer in 2017. As a first development-focused job, it was under my umbrella, making from ground-up WordPress templates for all companies in the group and internal management software.`,
+  },
+] as WorkExperienceData[];
