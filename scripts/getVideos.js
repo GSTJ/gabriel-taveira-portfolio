96261c1e7157d98ecbec1f374d8cee736166458f
Gabriel Taveira <gabrielstaveira@gmail.com>
2023-04-08 10:17:19 -0300

    fix: filter by space cast videos only

--- scripts/getVideos.js
+++ scripts/getVideos.js
@@ -4,11 +4,13 @@ const fs = require("fs");
 const getVideos = async () => {
   const videos = await getChannelVideos("UCQ_RmjT0te7n5RLlG381T5A");
 
-  const videosData = videos.map((video) => ({
-    name: video.title,
-    url: `https://www.youtube.com/watch?v=${video.id}`,
-    image: `https://i.ytimg.com/vi/${video.id}/maxresdefault.jpg`,
-  }));
+  const videosData = videos
+    .filter((video) => video.title.includes("Space Cast"))
+    .map((video) => ({
+      name: video.title,
+      url: `https://www.youtube.com/watch?v=${video.id}`,
+      image: `https://i.ytimg.com/vi/${video.id}/maxresdefault.jpg`,
+    }));
 
   fs.writeFileSync("src/app/consts/videos.json", JSON.stringify(videosData));
 };
