099d8fff7f7eb4f4c39ec876fff23b7b811d3d2f
Gabriel Taveira <gabrielstaveira@gmail.com>
2023-04-11 00:29:38 -0300

    fix: update GTM script

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -22,16 +22,14 @@ const GTM_ID = "G-1S8PR4TDYM";
 
 export default ({ children }: { children: React.ReactNode }) => (
   <html lang="en" className={poppins.className}>
-    <script
-      async
-      src="https://www.googletagmanager.com/gtag/js?id=G-1S8PR4TDYM"
-    />
     <Script id="google-tag-manager" strategy="afterInteractive">
-      {`window.dataLayer = window.dataLayer || [];
-      function gtag(){dataLayer.push(arguments);}
-      gtag('js', new Date());
-
-      gtag('config', '${GTM_ID}');`}
+      {`
+        (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+        new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+        j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+        'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+        })(window,document,'script','dataLayer','${GTM_ID}');
+      `}
     </Script>
     <body>{children}</body>
   </html>
