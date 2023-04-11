a4a3b64fa8aa32884b121564de533e87f46e2386
Gabriel Taveira <gabrielstaveira@gmail.com>
2023-04-10 21:07:33 -0300

    fix: ze delivery job description

--- src/app/consts/workExperiences.ts
+++ src/app/consts/workExperiences.ts
@@ -18,9 +18,9 @@ export default [
     jobType: "Contractor",
     technologies: ["React", "Node.JS"],
 
-    jobDescription: `At Meta, I work within Kustomer, a platform that allows businesses to manage all their communication channels in one place. Here, I develop applications and provide feedback on UX to improve the platform.
-    
-    Currently, I am working on a design system that will be open-sourced, enabling external developers to develop standardized applications that align with our brand.
+    jobDescription: `At Meta, I work within Kustomer, a platform that allows businesses to manage all their communication channels in one place. Here, I develop applications and provide feedback on UX to improve the platform. 
+
+    Currently, I am working on a design system that will be open-sourced, enabling external developers to develop standardized applications that align with our brand. 
     
     This makes our application review process easier, provides a better, faster and more consistent development experience, and offers a more consistent application experience for end-users.`,
   },
@@ -30,11 +30,11 @@ export default [
     subCompanyName: "Groundswell (California)",
     jobTitle: "Senior Software Engineer",
     technologies: ["React", "React Native", "Node.JS"],
-    jobDescription: `At X-Team, I work for a fintech company in California called Groundswell, developing a donation platform for businesses and employees. My focus is on the main application, which is built using React Native.
+    jobDescription: `X-Team works with big, innovative brands like Coinbase, Riot Games, Fox Broadcasting, Twitter, Sony, Intel, Beachbody, Kaplan Inc., and more.
+
+    At X-Team, I work for a fintech company in California called Groundswell, developing a donation platform for businesses and employees. My focus is on the main application, which is built using React Native. 
     
-    I aim to help people and promote a culture of robust testing to ensure that anyone can safely refactor our code.
-    
-    As a financial platform, we have implemented various security standards, such as SOC2. I am currently working on a complete platform rewrite, which involves switching from GO to Node.JS stack.`,
+    I aim to help people and promote a culture of robust testing to ensure that anyone can safely refactor our code. As a financial platform, we have implemented various security standards, such as SOC2. I am currently working on a complete platform rewrite, which involves switching from GO to Node.JS stack.`,
   },
   {
     startDate: "2020",
@@ -43,11 +43,15 @@ export default [
     subCompanyName: "Anheuser-Busch InBev",
     jobTitle: "Frontend Engineer III",
     technologies: ["React", "React Native", "Node.JS"],
-    jobDescription: `At X-Team, I work for a fintech company in California called Groundswell, developing a donation platform for businesses and employees. My focus is on the main application, which is built using React Native.
-      
-      I aim to help people and promote a culture of robust testing to ensure that anyone can safely refactor our code.
-      
-      As a financial platform, we have implemented various security standards, such as SOC2. I am currently working on a complete platform rewrite, which involves switching from GO to Node.JS stack.`,
+    jobDescription: `Zé Delivery is Brazil's largest beverage delivery app, with millions of users, powered by Anheuser-Busch InBev.
+
+    In this role, I contributed to the development of a better user experience in the main application as part of the Consumer Purchase team, focusing on improving user price perception and conversion.
+    
+    During this time, I reduced technical debt by refactoring the "browse" feature, which accounted for about 1/3 of the application, significantly decreasing development delivery time.
+    
+    I also developed a robust implementation for handling deep links, boosting our marketing campaigns, and assisted in creating Mozeic, our design system that is currently under construction.
+    
+    Additionally, I interviewed potential team members and created a new API for our Code Challenge using Node.js.`,
   },
   {
     startDate: "2019",
