1b8e409cd67404717d4a44d7210bfda0359c8342
Gabriel Taveira <gabrielstaveira@gmail.com>
2023-04-11 00:13:37 -0300

    feat: add more padding-bottom to links

--- src/app/links/page.tsx
+++ src/app/links/page.tsx
@@ -2,7 +2,7 @@ import { Section } from "@/components/Section";
 import { Socials } from "@/components/sections";
 
 export default () => (
-  <main className="flex flex-col items-center p-10 md:p-24">
+  <main className="flex flex-col items-center p-10 pb-14 md:p-24">
     <div className="flex flex-col items-center max-w-6xl">
       <Section.Root>
         <Section.Header step="Redes Sociais" title="Gabriel Taveira" />
