5270fd39f25037b9442d40d741f02dee5f7ee441
Gabriel Taveira <gabrielstaveira@gmail.com>
2023-04-11 00:26:28 -0300

    feat: insert GTM

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import "@total-typescript/ts-reset";
 import "./globals.css";
 
 import { Poppins } from "next/font/google";
+import Script from "next/script";
 
 export const metadata = {
   title: "Gabriel Taveira",
@@ -17,8 +18,21 @@ const poppins = Poppins({
   display: "swap",
 });
 
+const GTM_ID = "G-1S8PR4TDYM";
+
 export default ({ children }: { children: React.ReactNode }) => (
   <html lang="en" className={poppins.className}>
+    <script
+      async
+      src="https://www.googletagmanager.com/gtag/js?id=G-1S8PR4TDYM"
+    />
+    <Script id="google-tag-manager" strategy="afterInteractive">
+      {`window.dataLayer = window.dataLayer || [];
+      function gtag(){dataLayer.push(arguments);}
+      gtag('js', new Date());
+
+      gtag('config', '${GTM_ID}');`}
+    </Script>
     <body>{children}</body>
   </html>
 );
