15ab21a0012fedfe2f7aa017a40de9acb9e8cfeb
Gabriel Taveira <gabrielstaveira@gmail.com>
2023-04-27 22:59:15 -0300

    set up cronjob to update the youtube data

---
+++ src/api/cron.ts
@@ -0,0 +1,22 @@
+import type { NextApiRequest, NextApiResponse } from "next";
+
+export default async function handler(
+  request: NextApiRequest,
+  response: NextApiResponse
+) {
+  // Only run if the key is correct
+  if (request.query.key !== "plsRefetchYoutubeData") {
+    response.status(404).end();
+    return;
+  }
+
+  // Only run on Wednesdays
+  if (new Date().getDay() !== 3) {
+    response.status(404).end();
+    return;
+  }
+
+  await fetch(process.env.DEPLOY_URL);
+
+  response.status(200).json({ success: true });
+}
