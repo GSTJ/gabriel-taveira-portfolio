eddfbc5c47896522728b392325572ca739ba85ed
Gabriel Taveira <gabrielstaveira@gmail.com>
2023-05-04 16:21:47 -0300

    feat: update social links to include workshop data

--- src/consts/socials.ts
+++ src/consts/socials.ts
@@ -36,4 +36,16 @@ export default [
     name: "Space Cast",
     url: "https://www.youtube.com/playlist?list=PLRqjZNegpUk8KgQxIvOh49093Iu-vFc9V",
   },
+  {
+    name: "Figma Design System Workshop",
+    url: "https://www.figma.com/file/akUxU8w0nWgm4VtsdHGCgv/Projeto-Design-System-Contador-Digital?node-id=0%3A1&t=fkuHvgR20aBjXJCM-1",
+  },
+  {
+    name: "Github Repo Design System Workshop",
+    url: "https://github.com/GSTJ/design-system-example",
+  },
+  {
+    name: "Slides Design System Workshop",
+    url: "https://docs.google.com/presentation/d/1geAf0VrnB-LVac9F6Xhr8QD4CnLAA48_u1bsS1GlFZo/edit?usp=sharing",
+  },
 ] as SocialsData[];
