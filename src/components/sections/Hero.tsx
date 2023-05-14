b86ffa611db0c6971816f63990974d487c54b295
Gabriel Taveira <gabrielstaveira@gmail.com>
2023-05-14 01:19:11 -0300

    fix: make animated button work for all text sizes

--- src/components/sections/Hero.tsx
+++ src/components/sections/Hero.tsx
@@ -6,7 +6,7 @@ function AnimatedButton({ getInTouch }) {
       href="#socials"
       className="animate-fade-up-shine flex flex-1 md:flex-initial rounded-md text-md p-0.5 bg-gradient-to-br from-[#00000010] dark:from-[#ffffff70] group to-transparent font-semibold shadow-sm hover:bg-[#00000010] dark:hover:bg-[#ffffff30] transition-all"
     >
-      <div className="h-12 w-36 rounded-[0.31rem] bg-gradient-rotate from-pink-500 via-yellow-500 to-rose-500 blur-lg absolute z-[-1] group-hover:blur-xl transition-all" />
+      <div className="h-12 w-full rounded-[0.31rem] bg-gradient-rotate from-pink-500 via-yellow-500 to-rose-500 blur-lg absolute z-[-1] group-hover:blur-xl transition-all" />
       <p className="h-full w-full flex flex-1 rounded-[0.31rem] py-3.5 px-5 bg-[#fcf7fc] dark:bg-black dark:text-gray-50">
         {getInTouch}
       </p>
