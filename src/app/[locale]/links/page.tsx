ec3fbba274a7b3b22162d6f8969ba35d76154ec5
Gabriel Taveira <gabrielstaveira@gmail.com>
2023-05-27 11:22:06 -0300

    feat: improve links page layout

--- src/app/[locale]/links/page.tsx
+++ src/app/[locale]/links/page.tsx
@@ -2,14 +2,10 @@ import { Section } from "@/components/Section";
 import { Socials } from "@/components/sections";
 
 export default () => (
-  <main className="flex flex-col items-center p-10 py-20 md:p-24">
+  <main className="flex flex-col items-center p-10 pb-20 pt-0 md:p-24">
     <div className="flex flex-col items-center max-w-6xl">
       <Section.Root>
-        <Section.Header
-          className="mt-0"
-          step="Redes Sociais"
-          title="Gabriel Taveira"
-        />
+        <Section.Header step="Redes Sociais" title="Gabriel Taveira" />
         <Socials />
       </Section.Root>
     </div>
