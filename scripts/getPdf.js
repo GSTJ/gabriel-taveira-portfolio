d51228be44ac4423f291adb761ec12ca000c96cf
Gabriel Taveira <gabrielstaveira@gmail.com>
2023-09-06 16:09:47 -0300

    ci: stop running next on containers

--- scripts/getPdf.js
+++ scripts/getPdf.js
@@ -7,7 +7,7 @@ const getPdf = async () => {
     });
 
     const page = await browser.newPage();
-    await page.goto("http://localhost:3000/", { waitUntil: "networkidle0" });
+    await page.goto("https://www.gabrieltaveira.dev", { waitUntil: "networkidle0" });
 
     // Evaluate the actual height of the page body
     const bodyHandle = await page.$("body");
