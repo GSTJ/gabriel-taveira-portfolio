0dcd36e1da34663d685638d2953ce6a0fd9dfcd4
Gabriel Taveira <gabrielstaveira@gmail.com>
2024-01-09 16:10:09 -0300

    use en-us curriculum

--- scripts/getPdf.js
+++ scripts/getPdf.js
@@ -2,34 +2,38 @@ const fs = require("fs");
 const puppeteer = require("puppeteer");
 
 const getPdf = async () => {
-    const browser = await puppeteer.launch({
-        headless: "new",
-    });
+  const browser = await puppeteer.launch({
+    headless: "new",
+  });
 
-    const page = await browser.newPage();
-    await page.goto("http://gabrieltaveira.dev/", { waitUntil: "networkidle0" });
+  const page = await browser.newPage();
+  await page.goto("https://www.gabrieltaveira.com.br/en-US", {
+    waitUntil: "networkidle0",
+  });
 
-    // Evaluate the actual height of the page body
-    const bodyHandle = await page.$("body");
-    const { height } = await bodyHandle.boundingBox();
-    await bodyHandle.dispose();
+  // Evaluate the actual height of the page body
+  const bodyHandle = await page.$("body");
+  const { height } = await bodyHandle.boundingBox();
+  await bodyHandle.dispose();
 
-    // For some reason, the pdf has extra padding at the bottom.
-    // This is a hacky way to remove it.
-    const EXTRA_BOTTOM_PADDING = 500;
-    const WEB_WIDTH = 1200;
+  // For some reason, the pdf has extra padding at the bottom.
+  // This is a hacky way to remove it.
+  const EXTRA_BOTTOM_PADDING = 500;
+  const WEB_WIDTH = 1200;
 
-    const pdf = await page.pdf({
-        printBackground: true,
-        width: WEB_WIDTH,
-        height: Math.ceil(height - EXTRA_BOTTOM_PADDING),
-        pageRanges: "1", // Ensures that only the first page is printed.
-    });
+  const pdf = await page.pdf({
+    printBackground: true,
+    width: WEB_WIDTH,
+    height: Math.ceil(height - EXTRA_BOTTOM_PADDING),
+    pageRanges: "1", // Ensures that only the first page is printed.
+  });
 
-    await browser.close();
+  await browser.close();
 
-    // Save curriculum to disk
-    await new Promise(resolve => fs.writeFile("public/curriculum.pdf", pdf, resolve));
+  // Save curriculum to disk
+  await new Promise((resolve) =>
+    fs.writeFile("public/curriculum.pdf", pdf, resolve)
+  );
 };
 
 getPdf();
