301e3a21189c2e2a58d03228a83d655648fe2da8
Gabriel Taveira <gabrielstaveira@gmail.com>
2025-06-11 19:43:29 -0300

    fix

--- next.config.ts
+++ next.config.ts
@@ -3,12 +3,12 @@ import withNextIntl from "next-intl/plugin";
 
 const createNextIntl = withNextIntl(
   // This is the default (also the `src` folder is supported out of the box)
-  "./src/i18n/index.ts",
+  "./src/i18n/index.ts"
 );
 
 const nextConfig = {
   devIndicators: false,
-  images: { domains: ["i.ytimg.com"] },
+  images: { remotePatterns: [{ hostname: "i.ytimg.com" }] },
   experimental: {
     reactCompiler: true,
     optimizeCss: true,
