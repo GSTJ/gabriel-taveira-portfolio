0179c9db211145ccf67219a23826cf47ca65b2a3
Gabriel Taveira <gabrielstaveira@gmail.com>
2025-06-11 17:52:25 -0300

    fix

--- src/@types/i18n.d.ts
+++ src/@types/i18n.d.ts
@@ -1,2 +1,3 @@
-type Messages = typeof import("../i18n/locales/en-US.json");
+import type Messages from "../i18n/locales/en-US.json";
+// eslint-disable-next-line @typescript-eslint/no-empty-object-type
 declare interface IntlMessages extends Messages {}
